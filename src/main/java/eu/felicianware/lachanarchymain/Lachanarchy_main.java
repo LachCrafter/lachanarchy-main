dd5b0563ca9019579656f692f237878430678fbf
LachCrafter <lachcrafteryt@gmail.com>
2024-07-01 06:22:49 +0200

    Implemented CrystalDelay

--- src/main/java/eu/felicianware/lachanarchymain/Lachanarchy_main.java
+++ src/main/java/eu/felicianware/lachanarchymain/Lachanarchy_main.java
@@ -1,23 +1,24 @@
 package eu.felicianware.lachanarchymain;
 
+import eu.felicianware.lachanarchymain.listeners.CrystalDelay;
+import eu.felicianware.lachanarchymain.commands.DiscordCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Lachanarchy_main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("Lachanarchy_main is enabled");
-
         // Register commands.
-        this.getCommand("discord").setExecutor(new eu.felicianware.lachanarchymain.commands.DiscordCommand());
+        this.getCommand("discord").setExecutor(new DiscordCommand());
 
-        // Register listeners.
+        // Register listeners/events.
+        getServer().getPluginManager().registerEvents(new CrystalDelay(this), this);
 
-
+        System.out.println("Lachanarchy_main loaded");
     }
 
     @Override
     public void onDisable() {
-        System.out.println("Lachanarchy_main is disabled");
+        System.out.println("Lachanarchy_main unloaded");
     }
 }
---
+++ src/main/java/eu/felicianware/lachanarchymain/listeners/CrystalDelay.java
@@ -0,0 +1,39 @@
+package eu.felicianware.lachanarchymain.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.Plugin;
+
+public class CrystalDelay implements Listener {
+    private final Plugin plugin;
+
+    public CrystalDelay(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Block clickedBlock = event.getClickedBlock();
+            if (clickedBlock != null && clickedBlock.getType() == Material.END_CRYSTAL) {
+                long placeTime = System.currentTimeMillis();
+
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                    if (clickedBlock.getType() == Material.END_CRYSTAL) {
+                        long breakTime = System.currentTimeMillis();
+                        long timeDifference = breakTime - placeTime;
+
+                        if (timeDifference > 250) { // 250ms = 5 ticks
+                            event.setCancelled(true);
+                        }
+                    }
+                }, 5L);
+            }
+        }
+    }
+}
