b8dfdbcacf517e4a389f2719c93e9385508897a5
LachCrafter <lachcrafteryt@gmail.com>
2024-07-01 16:32:56 +0200

    Fixed discord command

--- src/main/java/eu/felicianware/lachanarchymain/commands/DiscordCommand.java
+++ src/main/java/eu/felicianware/lachanarchymain/commands/DiscordCommand.java
@@ -1,25 +1,31 @@
 package eu.felicianware.lachanarchymain.commands;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Formatter;
-
 public class DiscordCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("discord")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                player.sendMessage(ChatColor.GOLD + "You can join the Discord Server here:https://discord.gg/WrkrRYb3mj");
-            }
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be used by players.");
             return true;
         }
-        return false;
+
+        Player player = (Player) sender;
+
+        Component discordMessage = Component.text()
+                .append(Component.text("You can join the discord server here: ", NamedTextColor.GOLD))
+                .append(Component.newline())
+                .append(Component.text("https://discord.gg/WrkrRYb3mj", NamedTextColor.BLUE))
+                .build();
+
+        player.sendMessage(discordMessage);
+
+        return true;
     }
 }
