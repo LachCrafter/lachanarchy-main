b9f4d960dd3303598f3a4cce89aa3336a14b188f
LachCrafter <lachcrafteryt@gmail.com>
2024-07-12 17:09:06 +0200

    fix join and leave messages 2.0

--- src/main/java/eu/felicianware/lachanarchymain/listeners/JoinLeaveMessages.java
+++ src/main/java/eu/felicianware/lachanarchymain/listeners/JoinLeaveMessages.java
@@ -1,37 +1,42 @@
 package eu.felicianware.lachanarchymain.listeners;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
 
 public class JoinLeaveMessages implements Listener {
 
+    private final Plugin plugin;
+
     public JoinLeaveMessages(Plugin plugin) {
+        this.plugin = plugin;
     }
 
     @EventHandler
-    public void onJoin(final PlayerJoinEvent event) {
-        Player joiningPlayer = event.getPlayer();
-        Component joinMessage = Component.text(joiningPlayer.displayName() + " joined.", NamedTextColor.DARK_GRAY);
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Component joinMessage = Component.text()
+                .append(Component.text(player.getName(), NamedTextColor.GRAY))
+                .append(Component.text(" joined.", NamedTextColor.DARK_GRAY))
+                .build();
 
         Bukkit.getServer().sendMessage(joinMessage);
-
     }
 
     @EventHandler
-    public void onLeave(final PlayerQuitEvent event) {
-        Player leaveingPlayer = event.getPlayer();
-        Component leaveMessage = Component.text(leaveingPlayer.displayName() + " left.", NamedTextColor.DARK_GRAY);
+    public void onLeave(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        Component leaveMessage = Component.text()
+                .append(Component.text(player.getName(), NamedTextColor.GRAY))
+                .append(Component.text(" left.", NamedTextColor.DARK_GRAY))
+                .build();
 
         Bukkit.getServer().sendMessage(leaveMessage);
-
     }
-
-
 }
