62c31304412a2a0d233f86a52c7d89b4698642c1
LachCrafter <lachcrafteryt@gmail.com>
2024-07-15 14:54:08 +0200

    Update KillCommand

--- src/main/java/eu/felicianware/lachanarchymain/commands/KillCommand.java
+++ src/main/java/eu/felicianware/lachanarchymain/commands/KillCommand.java
@@ -4,17 +4,30 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * KillCommand is a command executor that allows a player to kill themselves.
+ */
 public class KillCommand implements CommandExecutor {
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    /**
+     * Executes the given command, returning its success.
+     *
+     * @param sender  Source of the command
+     * @param command Command which was executed
+     * @param label   Alias of the command which was used
+     * @param args    Passed command arguments
+     * @return true if a valid command, otherwise false
+     */
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage("This command can only be used by players.");
             return true;
         }
 
         Player player = (Player) sender;
-
         player.setHealth(0);
         return true;
     }
