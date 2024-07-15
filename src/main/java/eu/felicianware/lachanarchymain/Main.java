769ed61c3ee6c0295084badd056f0046c66e0f32
LachCrafter <lachcrafteryt@gmail.com>
2024-07-15 14:55:33 +0200

    deprecated

--- src/main/java/eu/felicianware/lachanarchymain/Main.java
+++ src/main/java/eu/felicianware/lachanarchymain/Main.java
@@ -3,7 +3,6 @@ package eu.felicianware.lachanarchymain;
 import eu.felicianware.lachanarchymain.commands.DiscordCommand;
 import eu.felicianware.lachanarchymain.commands.HelpCommand;
 import eu.felicianware.lachanarchymain.commands.KillCommand;
-import eu.felicianware.lachanarchymain.listeners.Antispam;
 import eu.felicianware.lachanarchymain.listeners.CrystalDelay;
 import eu.felicianware.lachanarchymain.listeners.JoinLeaveMessages;
 import eu.felicianware.lachanarchymain.listeners.RandomSpawn;
@@ -21,7 +20,6 @@ public final class Main extends JavaPlugin {
 
         // Register listeners
         getServer().getPluginManager().registerEvents(new CrystalDelay(this), this);
-        getServer().getPluginManager().registerEvents(new Antispam(this), this);
         getServer().getPluginManager().registerEvents(new RandomSpawn(this), this);
         getServer().getPluginManager().registerEvents(new JoinLeaveMessages(this), this);
 
